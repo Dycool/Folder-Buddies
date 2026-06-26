48d9d6e1216d4f5e586914a2e619e375dc8dac89
Diogo Enes <ist1113618@tecnico.ulisboa.pt>
2026-06-26 18:22:58 +0100

    too many arguments fix

--- client/include/fs/projfs_loader.h
+++ client/include/fs/projfs_loader.h
@@ -1,8 +1,8 @@
 #pragma once
 #ifdef _WIN32
 
-#include <projectedfslib.h>
 #include <windows.h>
+#include <projectedfslib.h>
 
 namespace fb {
 namespace projfs {
@@ -12,13 +12,13 @@ namespace projfs {
 bool ensure_loaded();
 
 // Function pointer typedefs matching the SDK signatures
-using MarkDirectoryAsPlaceholder_t = HRESULT(WINAPI*)(PCWSTR rootPath, PCWSTR targetPath, PCWSTR virtualizationInstanceID, PRJ_CALLBACK_DATA* callbackData);
-using StartVirtualizing_t = HRESULT(WINAPI*)(PCWSTR virtualizationRootPath, const PRJ_CALLBACKS* callbacks, void* instanceContext, const PRJ_STARTVIRTUALIZING_OPTIONS* options, PRJ_NAMESPACE_VIRTUALIZATION_CONTEXT* namespaceVirtualizationContext);
+using MarkDirectoryAsPlaceholder_t = HRESULT(WINAPI*)(PCWSTR rootPathName, PCWSTR targetPathName, const PRJ_PLACEHOLDER_VERSION_INFO* versionInfo, const GUID* virtualizationInstanceID);
+using StartVirtualizing_t = HRESULT(WINAPI*)(PCWSTR virtualizationRootPath, const PRJ_CALLBACKS* callbacks, const void* instanceContext, const PRJ_STARTVIRTUALIZING_OPTIONS* options, PRJ_NAMESPACE_VIRTUALIZATION_CONTEXT* namespaceVirtualizationContext);
 using StopVirtualizing_t = void(WINAPI*)(PRJ_NAMESPACE_VIRTUALIZATION_CONTEXT namespaceVirtualizationContext);
 using WritePlaceholderInfo_t = HRESULT(WINAPI*)(PRJ_NAMESPACE_VIRTUALIZATION_CONTEXT namespaceVirtualizationContext, PCWSTR destinationFileName, const PRJ_PLACEHOLDER_INFO* placeholderInfo, UINT32 placeholderInfoSize);
-using WriteFileData_t = HRESULT(WINAPI*)(PRJ_NAMESPACE_VIRTUALIZATION_CONTEXT namespaceVirtualizationContext, const PRJ_DATA_STREAM_ID* dataStreamId, const void* buffer, UINT64 byteOffset, UINT32 length);
-using FillDirEntryBuffer_t = HRESULT(WINAPI*)(PCWSTR fileName, const PRJ_FILE_BASIC_INFO* basicInfo, PRJ_DIR_ENTRY_BUFFER_HANDLE dirEntryBufferHandle);
-using FileNameMatch_t = BOOLEAN(WINAPI*)(PCWSTR fileNameToMatch, PCWSTR pattern);
+using WriteFileData_t = HRESULT(WINAPI*)(PRJ_NAMESPACE_VIRTUALIZATION_CONTEXT namespaceVirtualizationContext, const GUID* dataStreamId, void* buffer, UINT64 byteOffset, UINT32 length);
+using FillDirEntryBuffer_t = HRESULT(WINAPI*)(PCWSTR fileName, PRJ_FILE_BASIC_INFO* fileBasicInfo, PRJ_DIR_ENTRY_BUFFER_HANDLE dirEntryBufferHandle);
+using FileNameMatch_t = BOOLEAN(WINAPI*)(PCWSTR fileNameToCheck, PCWSTR pattern);
 
 // Resolved function pointers (nullptr if DLL not loaded or symbol missing)
 extern MarkDirectoryAsPlaceholder_t MarkDirectoryAsPlaceholder;
